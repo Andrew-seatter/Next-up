5c8df21c5d966ba592597553f22fa994ac768bea
LE Palmer <lpalmer@ucsd.edu>
2024-03-15 07:54:02 -0700

    added starting seed data, jobcard component for dashboard, and beginning css for dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,39 @@
-import styles from '../Dashboard/Dashboard.module.css';
+import mockJobs from "../../lib/mock-jobs";
+import { Grid } from "@mui/material";
+import JobCard from "./JobCard";
+/*
+    Array.map is an array method that returns a new array based on an old one.
+    const numbers = [1, 2, 3]
+    const doubles = numbers.map(n => n*2)
+        [2, 4, 6]
+
+    ``  template string
+    ${} template literal
+
+    Bootstrap breakpoints
+        xs
+        sm
+        md
+        lg
+        xl
+*/
 
 export const Dashboard = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}></h1>
-        </div>
-
-    );
-}
-
+  return (
+    <Grid container>
+      <Grid item xs={3}>
+        SIDE-BAR
+      </Grid>
+      <Grid item xs={9}>
+        <Grid container id="job-cards" spacing={2}>
+          {mockJobs.map((job, i) => {
+            const key = `${job.jobTitle}-${i}`;
+            return <JobCard key={key} job={job} />;
+          })}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Dashboard;
---
+++ client/src/lib/mock-jobs.js
@@ -0,0 +1,161 @@
+export default [{
+  "jobTitle": "Senior Sales Associate",
+  "companyName": "Buzzbean",
+  "dateApplied": "3/1/2024",
+  "status": "hired",
+  "stars": 1,
+  "notes": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+  "contact": "Bronson Breckon"
+}, {
+  "jobTitle": "Accountant III",
+  "companyName": "Devshare",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 2,
+  "notes": "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+  "contact": "Hestia Skakunas"
+}, {
+  "jobTitle": "Electrical Engineer",
+  "companyName": "Devshare",
+  "dateApplied": "3/1/2024",
+  "status": "interviewed",
+  "stars": 1,
+  "notes": "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+  "contact": "Rheba Dykas"
+}, {
+  "jobTitle": "Design Engineer",
+  "companyName": "Voolith",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 4,
+  "notes": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+  "contact": "Sergent Parlot"
+}, {
+  "jobTitle": "Senior Cost Accountant",
+  "companyName": "Jabbercube",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 4,
+  "notes": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+  "contact": "Vivyan Drews"
+}, {
+  "jobTitle": "Payment Adjustment Coordinator",
+  "companyName": "Topdrive",
+  "dateApplied": "3/1/2024",
+  "status": "hired",
+  "stars": 2,
+  "notes": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+  "contact": "Elianora Mervyn"
+}, {
+  "jobTitle": "Environmental Specialist",
+  "companyName": "Jetpulse",
+  "dateApplied": "3/1/2024",
+  "status": "interviewed",
+  "stars": 2,
+  "notes": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  "contact": "Antonetta Ioannou"
+}, {
+  "jobTitle": "Help Desk Operator",
+  "companyName": "Flashspan",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 1,
+  "notes": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+  "contact": "Maressa Paulusch"
+}, {
+  "jobTitle": "Editor",
+  "companyName": "Meejo",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 2,
+  "notes": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+  "contact": "Michaella Scatchar"
+}, {
+  "jobTitle": "Recruiting Manager",
+  "companyName": "Janyx",
+  "dateApplied": "3/1/2024",
+  "status": "hired",
+  "stars": 1,
+  "notes": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+  "contact": "Timoteo Bernardoux"
+}, {
+  "jobTitle": "Staff Scientist",
+  "companyName": "Gevee",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 5,
+  "notes": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
+  "contact": "Levi Yuryshev"
+}, {
+  "jobTitle": "Structural Analysis Engineer",
+  "companyName": "Jayo",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 4,
+  "notes": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+  "contact": "Zondra Oseman"
+}, {
+  "jobTitle": "Assistant Professor",
+  "companyName": "Kayveo",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 2,
+  "notes": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+  "contact": "Jillana Hanton"
+}, {
+  "jobTitle": "Statistician II",
+  "companyName": "Thoughtmix",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 3,
+  "notes": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+  "contact": "Blair Bradshaw"
+}, {
+  "jobTitle": "VP Quality Control",
+  "companyName": "Yakitri",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 2,
+  "notes": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+  "contact": "Alleen Pentony"
+}, {
+  "jobTitle": "Chemical Engineer",
+  "companyName": "Flipstorm",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 5,
+  "notes": "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+  "contact": "Kerstin Yoakley"
+}, {
+  "jobTitle": "Graphic Designer",
+  "companyName": "Thoughtworks",
+  "dateApplied": "3/1/2024",
+  "status": "hired",
+  "stars": 1,
+  "notes": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+  "contact": "Clive Gilfoyle"
+}, {
+  "jobTitle": "Executive Secretary",
+  "companyName": "Innojam",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 3,
+  "notes": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.",
+  "contact": "Carey Carbry"
+}, {
+  "jobTitle": "Budget/Accounting Analyst I",
+  "companyName": "Aivee",
+  "dateApplied": "3/1/2024",
+  "status": "pending",
+  "stars": 0,
+  "notes": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+  "contact": "Dyanna Grover"
+}, {
+  "jobTitle": "Account Executive",
+  "companyName": "Lajo",
+  "dateApplied": "3/1/2024",
+  "status": "rejected",
+  "stars": 5,
+  "notes": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+  "contact": "Howey Warrillow"
+}]
