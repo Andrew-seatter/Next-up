16146239691d6cdda61e99e0a907bb0ee539a9e0
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-26 17:09:06 -0700

    updated readme and resources page

--- client/src/components/Resources/Resources.jsx
+++ client/src/components/Resources/Resources.jsx
@@ -2,39 +2,33 @@ import { Grid, Typography, Link, Card, CardContent } from '@mui/material';
 import Sidebar from "../Navigation/Sidebar";
 import { useMediaQuery, useTheme } from '@mui/material';
 import MobileMenu from '../Navigation/MobileMenu';
+import styles from '../Resources/Resources.module.css'
+
 
 const resources = {
     'Resume Help': [
+        { title: 'How to Make a Resume', url: 'https://www.indeed.com/career-advice/resumes-cover-letters/how-to-make-a-resume-with-examples' },
         { title: 'How to Write a Resume - The Complete Guide', url: 'https://novoresume.com/career-blog/how-to-write-a-resume-guide' },
-
-        // Placeholder Resources
-        { title: 'Top Resume Mistakes to Avoid', url: 'https://example.com/resume-mistakes' },
-        { title: 'Resume Formatting Best Practices', url: 'https://example.com/resume-formatting' },
-        { title: 'Action Verbs to Make Your Resume Stand Out', url: 'https://example.com/action-verbs-resume' },
+        { title: 'Write an Incredible Resume - video', url: 'https://www.youtube.com/watch?v=Tt08KmFfIYQ' },
+        { title: 'The Best Professional Summary Formula for Resume and LinkedIn', url: 'https://www.youtube.com/watch?v=Bip6BXtOQ_I' },
     ],
     'Interview Tips': [
         { title: '10 Common Job Interview Questions and How to Answer Them', url: 'https://hbr.org/2021/11/10-common-job-interview-questions-and-how-to-answer-them'},
-
-        // Placeholder Resources
-        { title: 'How to Prepare for a Behavioral Interview', url: 'https://example.com/behavioral-interview-prep' },
-        { title: 'Dressing for Success: Interview Attire', url: 'https://example.com/interview-attire' },
-        { title: 'Mastering the Virtual Interview', url: 'https://example.com/virtual-interview-tips' },
+        { title: 'How to Not Be Nervous in Job Interviews', url: 'https://www.youtube.com/watch?v=9FSSu8Ix0PA' },
+        { title: 'Interview Questions and Answers: How to Ace an Interview', url: 'https://www.youtube.com/watch?v=TZ3C_syg9Ow' },
+        { title: '6 Interview Skills That Will Get You Hired', url: 'https://www.linkedin.com/business/learning/blog/job-seeking-tips/6-interview-skills-that-will-get-you-hired-2023' },
     ],
     'Job Search Strategies': [
         { title: '9 Job Search Strategies to Help you Find your Dream Job', url: 'https://www.indeed.com/career-advice/finding-a-job/job-search-strategy'},
-
-        // Placeholder Resources
-        { title: 'Leveraging LinkedIn for Job Searching', url: 'https://example.com/linkedin-job-search' },
-        { title: 'The Power of Networking in Job Hunting', url: 'https://example.com/networking-strategies' },
-        { title: 'How to Use Social Media in Your Job Search', url: 'https://example.com/social-media-job-search' },
+        { title: '15 Job Search Strategies that Work', url: 'https://www.forbes.com/sites/robinryan/2021/11/16/15-job-search-strategies-that-work/?sh=39ebcf785a08' },
+        { title: 'How to Master Your Job Search', url: 'https://www.youtube.com/watch?v=h_04pmxmHQc' },
+        { title: '3 Ideas for More Effective Job Hunting', url: 'https://www.youtube.com/watch?v=tVKfJ6z4fYg' },
     ],
     'Career Development': [
         { title: '12 tips for career development and advancement', url: 'https://www.intuit.com/blog/innovative-thinking/12-tips-for-career-development-and-advancement/'},
-
-        // Placeholder Resources
-        { title: 'Setting Career Goals and How to Achieve Them', url: 'https://example.com/setting-career-goals' },
-        { title: 'The Importance of Continuous Learning in Your Career', url: 'https://example.com/continuous-learning' },
-        { title: 'Mentorship: Finding a Mentor and Why It Matters', url: 'https://example.com/finding-a-mentor' },
+        { title: '5 Proven Strategies to Increase Your Visibility and Get Promoted At Work', url: 'https://www.youtube.com/watch?v=JFtKeKTpJo0' },
+        { title: 'Your Career Path Does Not Have to Be a Straight Line', url: 'https://www.youtube.com/watch?v=oAgMKap9Cv8' },
+        { title: 'How To Create A Career Development Plan', url: 'https://www.forbes.com/advisor/business/career-development-plan/' },
     ]
 };
 
@@ -53,15 +47,15 @@ export const Resources = () => {
                 </Grid>
             )}
 
-            <Grid item xs={10} id="dash" container spacing={2}>
+            <Grid item xs={10} container spacing={2} className={styles.resourcesContainer}>
                 {Object.entries(resources).map(([category, links]) => (
                     <Grid item xs={12} sm={6} key={category}>
-                        <Card>
+                        <Card className={styles.resourceCard}>
                             <CardContent>
                                 <Typography variant="h5" gutterBottom>
                                     {category}
                                 </Typography>
-                                <ul>
+                                <ul className={styles.resourceList}>
                                     {links.map((link, index) => (
                                         <li key={index}>
                                             <Link href={link.url} target="_blank" rel="noopener noreferrer">
